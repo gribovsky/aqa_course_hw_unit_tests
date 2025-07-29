abd24aa444ab645b265f727456a252f5f5fc6e7a
gribovsky <m.gribovsky@gmail.com>
2025-07-29 14:59:01 +0300

    Optional task solved. Solution placed in homework folder

--- homework-solutions/js/hw-3/task-2.js
+++ homework-solutions/js/hw-3/task-2.js
@@ -4,3 +4,8 @@
 n + nn + nnn, где n не перемножаются, а конкатенируются
 
 */
+let n = 1 + Math.floor(Math.random()*9);
+let secondNumber = Number(String(n) + n);
+let thirdNumber =  Number(String(n) + n + n);
+console.log(n +'\n',"secondNumber == " + secondNumber +'\n',"thirdNumber == " + thirdNumber);
+console.log(n + secondNumber + thirdNumber);
