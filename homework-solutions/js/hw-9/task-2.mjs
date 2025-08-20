a2006c7835a3bd8aff31004c2336a9ef2120b017
gribovsky <m.gribovsky@gmail.com>
2025-08-20 15:10:27 +0300

    HW 9

--- homework-solutions/js/hw-9/task-2.mjs
+++ homework-solutions/js/hw-9/task-2.mjs
@@ -9,28 +9,62 @@
 
 const characters = [
   { name: 'Barney', age: 35 },
-  { name: 'Fred', age: 39 },
+  { name: 'Fred', age: 40 },
   { name: 'Jack', age: 49 },
 ];
 
 function addCharacter(character) {
   // Ваш код
+  if (typeof character !== 'object' || typeof character.name !== 'string' || typeof character.age !== 'number') {
+    throw new Error('Некорректный ввод: Объект должен иметь поля name (string) и age (number)');
+  }
+  return characters.push(character);
 }
 
 function getCharacter(name) {
-  // Ваш код
+  for (const obj of characters) {
+    if (obj.name === name) {
+      return obj;
+    }
+  }
+  // вариант через reduce (но церез цикл как-то проще)
+  // return characters.reduce((result, obj) => {
+  //     if (obj.name === name) {
+  //       result = obj;
+  //     } return result
+  //   }, undefined);
 }
 
 function getCharactersByAge(minAge) {
   // Ваш код
+  if (typeof minAge !== 'number') {
+    throw new Error('Некорректный ввод');
+  }
+  return characters.reduce((result, obj) => {
+    if (obj.age >= minAge) {
+      result.push(obj);
+    }
+    return result;
+  }, []);
 }
 
 function updateCharacter(name, newCharacter) {
   // Ваш код
+  const oldCharacter = getCharacter(name);
+  if (!oldCharacter) {
+    throw new Error(`Character ${name} isn't found`);
+  }
+  oldCharacter.name = newCharacter.name;
+  oldCharacter.age = newCharacter.age;
+  return characters;
 }
 
 function removeCharacter(name) {
   // Ваш код
+  const ind = characters.findIndex((obj) => obj.name === name);
+  if (ind === -1) {
+    throw new Error('Character is not found');
+  } else return characters.splice(ind, 1);
 }
 
 export { characters, addCharacter, updateCharacter, getCharacter, getCharactersByAge, removeCharacter };
